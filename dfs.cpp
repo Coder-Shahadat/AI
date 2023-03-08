580cbe82b632c0d393b67e5ee75abcec42763c72
MD Shahadat Hossain Bhuian <103915287+Coder-Shahadat@users.noreply.github.com>
2023-03-08 20:00:53 +0600

    Add files via upload

--- dfs.cpp
+++ dfs.cpp
@@ -4,24 +4,25 @@ using namespace std;
 const int Max = 1e5;
 bool visited[Max] = {false};
 void dfs(int v,vector<int>g[]){
-    visited[v] = true; 
+    visited[v] = true;
     cout << "-->" << v;
     for(auto child:g[v]){
-        if(!visited[child])
-            dfs(child,g);
+        if(visited[child]==false)
+            dfs(child, g);
     }
 }
 
 int main(){
-    int node, edges;
-    cin >> node >> edges; 
-    vector<int> g[node];
-    for (int i = 0; i < edges;i++){
+    int nodes, edges;
+    cin >> nodes >> edges;
+    vector<int> g[nodes + 1];
+    while(edges--){
         int u, v;
         cin >> u >> v;
         g[u].push_back(v);
         g[v].push_back(u);
     }
     cout << "start";
-    dfs(0,g);
+    dfs(2, g);
+    return 0;
 }
